2e17476a48d3b496e68329eb25741104a5e0ddaf
Will Dalebroux <dalebrouxw@msoe.edu>
2025-11-10 22:38:32 -0600

    part 4

--- recv_signal.c
+++ recv_signal.c
@@ -1,11 +1,42 @@
 /**
- * File: 
- * Modified by:
+ * File: recv_signal.c  
+ * Modified by: Will Dalebroux
  * 
  * Brief summary of program:
+ * handles signals sent from send_signal.c, prints recieved signal and value sent from
+ * send_signal.c
+ * Note: run with ./recv_signal &
+ * Note 2: terminate process with kill -SIGKILL <pid> (in commandline)
  */
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+
+void handle_usr1(int sig, siginfo_t *info, void *context)
+{
+    printf("got SIGUSR1 from pid %d, value=%d\n", info->si_pid, info->si_value.sival_int);
+}
 
 int main(void)
 {
+    struct sigaction sa; // declare struct to hold signal handler instructions
+    sa.sa_flags = SA_SIGINFO; // enable detailed info
+    sa.sa_sigaction = handle_usr1; // use handler
+    sigemptyset(&sa.sa_mask); // don't block other signals
+
+    if(sigaction(SIGUSR1, &sa, NULL) == -1) // register handler
+    {
+        perror("sigaction");
+        return 1;
+    }
+
+    printf("receiving pid: %d (waiting for SIGUSR1)\n", getpid());
+
+    while(1)
+    {
+        sleep(1); // pause indefinitely to run in background
+    }
+
     return 0;
 }
--- send_signal.c
+++ send_signal.c
@@ -1,11 +1,39 @@
 /**
- * File: 
- * Modified by:
+ * File: send_signal.c  
+ * Modified by: Will Dalebroux
  * 
  * Brief summary of program:
+ * take in pid from commandline arg, generates random number to send to receiving process,
+ * queues then sends random number to receiving process
  */
+ 
 
- int main(void)
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <signal.h>
+ #include <time.h>
+
+ int main(int argc, char *argv[])
  {
-    return 0;
+   if(argc != 2)
+   {
+      fprintf(stderr, "Usage: %s <receiving pid>\n", argv[0]);
+      return 2;
+   }
+
+   pid_t pid = (pid_t)strtol(argv[1], NULL, 10); // convert argument from string to pid_t
+
+   srand((unsigned)time(NULL)); // seed
+   int value = rand() % 1000000; // random value to send
+
+   union sigval sv; // declare union to store sending value in 
+   sv.sival_int = value; //include value while sending signal
+
+   printf("sending value %d to pid %d through SIGUSR1\n", value, pid);
+   if(sigqueue(pid, SIGUSR1, sv) == -1) // find pid, verify signal can be sent, queue signal to be sent, store attached int in sv
+   {
+      perror("sigqueue");
+      return 1;
+   }
+   return 0;
  }
